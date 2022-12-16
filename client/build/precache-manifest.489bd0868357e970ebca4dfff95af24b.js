dd767b9dd310da65e709a271cc769bbec0e1058a
sj212131 <sj212131@hotmail.com>
2022-12-16 13:48:49 -0500

    update deploy

---
+++ client/build/precache-manifest.489bd0868357e970ebca4dfff95af24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64aa9f69022890afd584",
+    "url": "/static/css/main.5f0adcc1.chunk.css"
+  },
+  {
+    "revision": "64aa9f69022890afd584",
+    "url": "/static/js/main.243efe20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "193b767e333ca6393c61",
+    "url": "/static/js/2.3bb06115.chunk.js"
+  },
+  {
+    "revision": "0b3a4bf4857f37c7966d5704adfbc6b3",
+    "url": "/static/media/BGimage.0b3a4bf4.jpg"
+  },
+  {
+    "revision": "6f9c1cce1ed9cc4f6a425a43d2c478e1",
+    "url": "/index.html"
+  }
+];
